de86bd8d119371f5f0d3ac820df3932071f2f67d
ankit1633 <111997421+ankit1633@users.noreply.github.com>
2024-08-31 10:18:19 +0530

    first

--- src/home.js
+++ src/home.js
@@ -1,306 +1,18 @@
-// import { useState, useEffect } from "react";
-// import { makeStyles, withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import Container from "@material-ui/core/Container";
-// import React from "react";
-// import Card from "@material-ui/core/Card";
-// import CardContent from "@material-ui/core/CardContent";
-// import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress } from "@material-ui/core";
-// import cblogo from "./cblogo.PNG";
-// import image from "./bg.png";
-// import { DropzoneArea } from 'material-ui-dropzone';
-// import { common } from '@material-ui/core/colors';
-// import Clear from '@material-ui/icons/Clear';
-
-
-
-
-// const ColorButton = withStyles((theme) => ({
-//   root: {
-//     color: theme.palette.getContrastText(common.white),
-//     backgroundColor: common.white,
-//     '&:hover': {
-//       backgroundColor: '#ffffff7a',
-//     },
-//   },
-// }))(Button);
-// const axios = require("axios").default;
-
-// const useStyles = makeStyles((theme) => ({
-//   grow: {
-//     flexGrow: 1,
-//   },
-//   clearButton: {
-//     width: "-webkit-fill-available",
-//     borderRadius: "15px",
-//     padding: "15px 22px",
-//     color: "#000000a6",
-//     fontSize: "20px",
-//     fontWeight: 900,
-//   },
-//   root: {
-//     maxWidth: 345,
-//     flexGrow: 1,
-//   },
-//   media: {
-//     height: 400,
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     margin: 'auto',
-//     maxWidth: 500,
-//   },
-//   gridContainer: {
-//     justifyContent: "center",
-//     padding: "4em 1em 0 1em",
-//   },
-//   mainContainer: {
-//     backgroundImage: `url(${image})`,
-//     backgroundRepeat: 'no-repeat',
-//     backgroundPosition: 'center',
-//     backgroundSize: 'cover',
-//     height: "93vh",
-//     marginTop: "8px",
-//   },
-//   imageCard: {
-//     margin: "auto",
-//     maxWidth: 400,
-//     height: 500,
-//     backgroundColor: 'transparent',
-//     boxShadow: '0px 9px 70px 0px rgb(0 0 0 / 30%) !important',
-//     borderRadius: '15px',
-//   },
-//   imageCardEmpty: {
-//     height: 'auto',
-//   },
-//   noImage: {
-//     margin: "auto",
-//     width: 400,
-//     height: "400 !important",
-//   },
-//   input: {
-//     display: 'none',
-//   },
-//   uploadIcon: {
-//     background: 'white',
-//   },
-//   tableContainer: {
-//     backgroundColor: 'transparent !important',
-//     boxShadow: 'none !important',
-//   },
-//   table: {
-//     backgroundColor: 'transparent !important',
-//   },
-//   tableHead: {
-//     backgroundColor: 'transparent !important',
-//   },
-//   tableRow: {
-//     backgroundColor: 'transparent !important',
-//   },
-//   tableCell: {
-//     fontSize: '22px',
-//     backgroundColor: 'transparent !important',
-//     borderColor: 'transparent !important',
-//     color: '#000000a6 !important',
-//     fontWeight: 'bolder',
-//     padding: '1px 24px 1px 16px',
-//   },
-//   tableCell1: {
-//     fontSize: '14px',
-//     backgroundColor: 'transparent !important',
-//     borderColor: 'transparent !important',
-//     color: '#000000a6 !important',
-//     fontWeight: 'bolder',
-//     padding: '1px 24px 1px 16px',
-//   },
-//   tableBody: {
-//     backgroundColor: 'transparent !important',
-//   },
-//   text: {
-//     color: 'white !important',
-//     textAlign: 'center',
-//   },
-//   buttonGrid: {
-//     maxWidth: "416px",
-//     width: "100%",
-//   },
-//   detail: {
-//     backgroundColor: 'white',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   appbar: {
-//     background: '#be6a77',
-//     boxShadow: 'none',
-//     color: 'white'
-//   },
-//   loader: {
-//     color: '#be6a77 !important',
-//   }
-// }));
-// export const ImageUpload = () => {
-//   const classes = useStyles();
-//   const [selectedFile, setSelectedFile] = useState();
-//   const [preview, setPreview] = useState();
-//   const [data, setData] = useState();
-//   const [image, setImage] = useState(false);
-//   const [isLoading, setIsloading] = useState(false);
-//   let confidence = 0;
-
-//   const sendFile = async () => {
-//     if (image) {
-//       let formData = new FormData();
-//       formData.append("file", selectedFile);
-//       let res = await axios({
-//         method: "post",
-//         url: "http://localhost:8000/predict",
-//         data: formData,
-//       });
-//       if (res.status === 200) {
-//         setData(res.data);
-//       }
-//       setIsloading(false);
-//     }
-//   }
-
-//   const clearData = () => {
-//     setData(null);
-//     setImage(false);
-//     setSelectedFile(null);
-//     setPreview(null);
-//   };
-
-//   useEffect(() => {
-//     if (!selectedFile) {
-//       setPreview(undefined);
-//       return;
-//     }
-//     const objectUrl = URL.createObjectURL(selectedFile);
-//     setPreview(objectUrl);
-//   }, [selectedFile]);
-
-//   useEffect(() => {
-//     if (!preview) {
-//       return;
-//     }
-//     setIsloading(true);
-//     sendFile();
-//   }, [preview]);
-
-//   const onSelectFile = (files) => {
-//     if (!files || files.length === 0) {
-//       setSelectedFile(undefined);
-//       setImage(false);
-//       setData(undefined);
-//       return;
-//     }
-//     setSelectedFile(files[0]);
-//     setData(undefined);
-//     setImage(true);
-//   };
-
-//   if (data) {
-//     confidence = (parseFloat(data.confidence) * 100).toFixed(2);
-//   }
-
-//   return (
-//     <React.Fragment>
-//       <AppBar position="static" className={classes.appbar}>
-//         <Toolbar>
-//           <Typography className={classes.title} variant="h6" noWrap>
-//             Krishi 
-//           </Typography>
-//           <div className={classes.grow} />
-//           {/* <Avatar src={cblogo}></Avatar> */}
-//         </Toolbar>
-//       </AppBar>
-//       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
-//         <Grid
-//           className={classes.gridContainer}
-//           container
-//           direction="row"
-//           alignItems="center"
-//           spacing={2}
-//         >
-//           <Grid item xs={12}>
-//             <Card className={`${classes.imageCard} ${!image ? classes.imageCardEmpty : ''}`}>
-//               {image && <CardActionArea>
-//                 <CardMedia
-//                   className={classes.media}
-//                   image={preview}
-//                   component="image"
-//                   title="Contemplative Reptile"
-//                 />
-//               </CardActionArea>
-//               }
-//               {!image && <CardContent className={classes.content}>
-//                 <DropzoneArea
-//                   acceptedFiles={['image/*']}
-//                   dropzoneText={"Drag and drop an image of leaf to process"}
-//                   onChange={onSelectFile}
-//                 />
-//               </CardContent>}
-//               {data && <CardContent className={classes.detail}>
-//                 <TableContainer component={Paper} className={classes.tableContainer}>
-//                   <Table className={classes.table} size="small" aria-label="simple table">
-//                     <TableHead className={classes.tableHead}>
-//                       <TableRow className={classes.tableRow}>
-//                         <TableCell className={classes.tableCell1}>Label:</TableCell>
-//                         <TableCell align="right" className={classes.tableCell1}>Confidence:</TableCell>
-//                       </TableRow>
-//                     </TableHead>
-//                     <TableBody className={classes.tableBody}>
-//                       <TableRow className={classes.tableRow}>
-//                         <TableCell component="th" scope="row" className={classes.tableCell}>
-//                           {data.class}
-//                         </TableCell>
-//                         <TableCell align="right" className={classes.tableCell}>{confidence}%</TableCell>
-//                       </TableRow>
-//                     </TableBody>
-//                   </Table>
-//                 </TableContainer>
-//               </CardContent>}
-//               {isLoading && <CardContent className={classes.detail}>
-//                 <CircularProgress color="secondary" className={classes.loader} />
-//                 <Typography className={classes.title} variant="h6" noWrap>
-//                   Processing
-//                 </Typography>
-//               </CardContent>}
-//             </Card>
-//           </Grid>
-//           {data &&
-//             <Grid item className={classes.buttonGrid} >
-
-//               <ColorButton variant="contained" className={classes.clearButton} color="primary" component="span" size="large" onClick={clearData} startIcon={<Clear fontSize="large" />}>
-//                 Clear
-//               </ColorButton>
-//             </Grid>}
-//         </Grid >
-//       </Container >
-//     </React.Fragment >
-//   );
-// };
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
 import Container from "@material-ui/core/Container";
 import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress, Select, MenuItem, InputLabel, FormControl } from "@material-ui/core";
-import cblogo from "./cblogo.PNG";
 import image from "./bg.png";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { common } from '@material-ui/core/colors';
 import Clear from '@material-ui/icons/Clear';
+import axios from "axios";
 
 const ColorButton = withStyles((theme) => ({
   root: {
@@ -311,8 +23,6 @@ const ColorButton = withStyles((theme) => ({
     },
   },
 }))(Button);
-
-const axios = require("axios").default;
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -326,17 +36,8 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "20px",
     fontWeight: 900,
   },
-  root: {
-    maxWidth: 345,
-    flexGrow: 1,
-  },
   media: {
     height: 400,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
   },
   gridContainer: {
     justifyContent: "center",
@@ -360,11 +61,6 @@ const useStyles = makeStyles((theme) => ({
   },
   imageCardEmpty: {
     height: 'auto',
-  },
-  noImage: {
-    margin: "auto",
-    width: 400,
-    height: "400 !important",
   },
   input: {
     display: 'none',
@@ -439,12 +135,13 @@ export const ImageUpload = () => {
   const [preview, setPreview] = useState();
   const [data, setData] = useState();
   const [image, setImage] = useState(false);
-  const [isLoading, setIsloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [type, setType] = useState('potato');
   let confidence = 0;
 
-  const sendFile = async () => {
-    if (image) {
+  // Memoize the sendFile function to avoid unnecessary re-renders
+  const sendFile = useCallback(async () => {
+    if (image && selectedFile) {
       let formData = new FormData();
       formData.append("file", selectedFile);
       try {
@@ -459,11 +156,11 @@ export const ImageUpload = () => {
         }
       } catch (error) {
         console.error("Error sending file:", error);
-        // You might want to set an error state here to display to the user
+        // Handle error (e.g., set error state)
       }
-      setIsloading(false);
+      setIsLoading(false);
     }
-  }
+  }, [image, selectedFile, type]);
 
   const clearData = () => {
     setData(null);
@@ -479,15 +176,20 @@ export const ImageUpload = () => {
     }
     const objectUrl = URL.createObjectURL(selectedFile);
     setPreview(objectUrl);
+
+    // Cleanup function to revoke the object URL
+    return () => {
+      URL.revokeObjectURL(objectUrl);
+    };
   }, [selectedFile]);
 
   useEffect(() => {
     if (!preview) {
       return;
     }
-    setIsloading(true);
+    setIsLoading(true);
     sendFile();
-  }, [preview]);
+  }, [preview, sendFile]); // Include sendFile in the dependency array
 
   const onSelectFile = (files) => {
     if (!files || files.length === 0) {
@@ -534,7 +236,6 @@ export const ImageUpload = () => {
                 onChange={(e) => setType(e.target.value)}
               >
                 <MenuItem value="potato">Potato</MenuItem>
-                
                 <MenuItem value="tomato">Tomato</MenuItem>
               </Select>
             </FormControl>
@@ -562,38 +263,36 @@ export const ImageUpload = () => {
                   <Table className={classes.table} size="small" aria-label="simple table">
                     <TableHead className={classes.tableHead}>
                       <TableRow className={classes.tableRow}>
-                        <TableCell className={classes.tableCell1}>Label:</TableCell>
-                        <TableCell align="right" className={classes.tableCell1}>Confidence:</TableCell>
+                        <TableCell className={classes.tableCell}>Name</TableCell>
+                        <TableCell className={classes.tableCell1}>Confidence</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody className={classes.tableBody}>
                       <TableRow className={classes.tableRow}>
-                        <TableCell component="th" scope="row" className={classes.tableCell}>
-                          {data.class}
-                        </TableCell>
-                        <TableCell align="right" className={classes.tableCell}>{confidence}%</TableCell>
+                        <TableCell className={classes.tableCell}>{data.name}</TableCell>
+                        <TableCell className={classes.tableCell1}>{confidence}%</TableCell>
                       </TableRow>
                     </TableBody>
                   </Table>
                 </TableContainer>
+                <br />
+                <ColorButton
+                  variant="contained"
+                  color="secondary"
+                  className={classes.clearButton}
+                  startIcon={<Clear />}
+                  onClick={clearData}
+                >
+                  Clear
+                </ColorButton>
               </CardContent>}
               {isLoading && <CardContent className={classes.detail}>
-                <CircularProgress color="secondary" className={classes.loader} />
-                <Typography className={classes.title} variant="h6" noWrap>
-                  Processing
-                </Typography>
+                <CircularProgress className={classes.loader} />
               </CardContent>}
             </Card>
           </Grid>
-          {data &&
-            <Grid item className={classes.buttonGrid} >
-              <ColorButton variant="contained" className={classes.clearButton} color="primary" component="span" size="large" onClick={clearData} startIcon={<Clear fontSize="large" />}>
-                Clear
-              </ColorButton>
-            </Grid>}
-        </Grid >
-      </Container >
-    </React.Fragment >
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 };
-
